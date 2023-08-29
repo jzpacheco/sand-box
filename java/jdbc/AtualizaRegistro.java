94d5dad75140d245ffe3886b02744eb36a3f691c
jzpacheco <jeffersonpacheco@hotmail.com>
2023-08-28 23:55:03 -0300

    More JDBC

--- java/jdbc/AtualizaRegistro.java
+++ java/jdbc/AtualizaRegistro.java
@@ -1,7 +1,29 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class AtualizaRegistro {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+        Connection conexao = Conexao.getConexao();
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Código pra pesquisa: ");
+        String codigo = entrada.nextLine();
+
+        System.out.println("Novo nome: ");
+        String novoNome = entrada.nextLine();
+
+        String sql = "UPDATE pessoas SET nome = ? WHERE codigo = ?";
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.setString(1,novoNome);
+        stmt.setString(2, codigo);
+        stmt.execute();
+
+        stmt.close();
+        conexao.close();
+        entrada.close();
     }
 }
